985513bbcec898adfaf46e889d55ab36527ca667
LancelotThore <lancelot.thore@etu.unilim.fr>
2023-12-21 23:48:36 +0100

    it-2

--- js/class/event.js
+++ js/class/event.js
@@ -85,10 +85,10 @@ class Event {
     }
 
     calculateWeek() {
-        var date = new Date(this.#start.getTime());
+        let date = new Date(this.#start.getTime());
         date.setHours(0, 0, 0, 0);
         date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-        var week1 = new Date(date.getFullYear(), 0, 4);
+        let week1 = new Date(date.getFullYear(), 0, 4);
         return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
             - 3 + (week1.getDay() + 6) % 7) / 7);
     }
